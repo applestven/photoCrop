774f50c2a23171a6448246e86d5c4e2569ba5848
applestven <applestven@gmail.com.com>
2023-05-07 22:24:50 +0800

    123123

--- koaCrop.sh
+++ koaCrop.sh
@@ -1,11 +1,17 @@
-docker stop mariadb-crop
-docker rm mariadb-crop
-docker pull applestven/mariadb-crop:latest
+# docker stop mariadb-crop
+# docker rm mariadb-crop
+# docker pull applestven/mariadb-crop:latest
 # docker run -d -p 8282:3000 --name nodejstest applestven/nodejstest:latest sh -c "npm i && pm2 start /service/koa/src/main.js --no-daemon"
 # docker run -d -p 3306:3306 --name mariadb-crop 
 # docker run -d -p 3306:3306 --name mariadb-crop -v /path/on/host:/var/lib/mysql applestven/mariadb-crop:latest 
 
-docker run -d -e MYSQL_USER=nextcloud -e MYSQL_ROOT_PASSWORD=12345687 -e MYSQL_DATABASE=nextcloud -p 3306:3306 --name mariadb-crop mariadb:latest
+# docker run -d -e MYSQL_USER=nextcloud -e MYSQL_ROOT_PASSWORD=12345687 -e MYSQL_DATABASE=nextcloud -p 3306:3306 --name mariadb-crop mariadb:latest
+
+if docker inspect --format '{{.State.Running}}' mariadb-crop; then
+    echo "Container is already running"
+else
+    docker run -d -e MYSQL_USER=nextcloud -e MYSQL_ROOT_PASSWORD=12345687 -e MYSQL_DATABASE=nextcloud -p 3306:3306 --name mariadb-crop mariadb:latest
+fi
 
 
 
@@ -21,3 +27,8 @@ docker rm nextcloud-crop
 docker pull applestven/nextcloud-crop:latest
 # docker run -d -p 8282:3000 --name nodejstest applestven/nodejstest:latest sh -c "npm i && pm2 start /service/koa/src/main.js --no-daemon"
 docker run -d -p 8080:80 --name nextcloud-crop applestven/nextcloud-crop:latest
+
+
+# 要停止所有的Docker镜像，可以使用以下命令 docker stop $(docker ps -aq)  
+# 删除所有容器 docker rm $(docker ps -aq)
+# 可以使用以下命令来删除所有镜像 docker rmi $(docker images -q)
