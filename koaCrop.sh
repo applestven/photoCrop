6b1071bc17f2f951549132363a0860511aecbc3c
applestven <applestven@gmail.com.com>
2023-05-07 21:21:24 +0800

    123

--- koaCrop.sh
+++ koaCrop.sh
@@ -3,7 +3,9 @@ docker rm mariadb-crop
 docker pull applestven/mariadb-crop:latest
 # docker run -d -p 8282:3000 --name nodejstest applestven/nodejstest:latest sh -c "npm i && pm2 start /service/koa/src/main.js --no-daemon"
 # docker run -d -p 3306:3306 --name mariadb-crop 
-docker run -d -p 3306:3306 --name mariadb-crop -v /path/on/host:/var/lib/mysql applestven/mariadb-crop:latest
+# docker run -d -p 3306:3306 --name mariadb-crop -v /path/on/host:/var/lib/mysql applestven/mariadb-crop:latest 
+
+docker run -d -e MYSQL_USER=nextcloud -e MYSQL_ROOT_PASSWORD=12345687 -e ENV MYSQL_DATABASE=nextcloud -p 3306:3306 --name mariadb:latest
 
 
 
