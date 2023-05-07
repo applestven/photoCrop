8892a812161a017142047ec5a95e19d57ffeae06
applestven <applestven@gmail.com.com>
2023-05-07 23:49:48 +0800

    222

--- koaCrop.sh
+++ koaCrop.sh
@@ -10,6 +10,7 @@
 if docker inspect --format '{{.State.Running}}' mariadb-crop; then
     echo "Container is already running"
 else
+    docker rm koa-crop
     docker run -d -e MYSQL_USER=nextcloud -e MYSQL_ROOT_PASSWORD=12345687 -e MYSQL_DATABASE=nextcloud -p 3306:3306 --name mariadb-crop mariadb:latest
 fi
 
@@ -31,6 +32,7 @@ docker run -d -p 8282:3000 --name koa-crop applestven/koa-crop:latest
 if docker inspect --format '{{.State.Running}}' nextcloud-crop; then
     echo "nextcloud-crop is already running"
 else
+    docker rm nextcloud-crop
     docker pull applestven/nextcloud-crop:latest
     docker run -d -p 8080:80 --name nextcloud-crop applestven/nextcloud-crop:latest
 fi
