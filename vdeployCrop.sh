ab726f5befdaedd80ee9241e0dd8f9db634356e3
applestven <applestven@gmail.com.com>
2023-05-07 16:32:28 +0800

    1222

--- vdeployCrop.sh
+++ vdeployCrop.sh
@@ -1,6 +1,6 @@
 ## 放在linux服务器上的文件   applestven是dockerHub用户名  $1是需要填的密码
-echo -e "---------docker Login--------"
-docker login --username=$1  --password=$2 # 这个是yml文件最后一行所传递的参数，你docker的用户名和密码
+# echo -e "---------docker Login--------"
+# docker login --username=$1  --password=$2 # 这个是yml文件最后一行所传递的参数，你docker的用户名和密码
 echo -e "---------docker Stop--------"
 docker stop vue-crop
 echo -e "---------docker Rm--------"
